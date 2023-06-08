dceb45e2c7b335fbd041d6373131895514277056
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2023-06-08 03:54:48 +0300

    Add files via upload

---
+++ HW9/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp1
+{
+	internal class Program
+	{
+		
+		static void Main(string[] args)
+		{
+
+			string text, pattern;
+			Console.WriteLine("enter text");
+			text = Console.ReadLine().ToLower();
+			Console.WriteLine("enter pattern");
+			pattern = Console.ReadLine().ToLower();
+			Console.Clear();
+
+			string[] words = text.Split(" ");
+
+			if (pattern.Contains("*"))
+			{
+
+				for (int i = 0; i < words.Length; i++)
+				{
+					bool flag = true;
+					for (int j = 0; j < pattern.Length; j++)
+					{
+						if (pattern[j] != '*' && !words[i].Contains(pattern[j]))
+						{
+							flag = false;
+						}
+					}
+					if (flag)
+						Console.WriteLine(words[i]);
+				}
+
+			}
+			else
+			{
+				for (int i = 0; i < words.Length; i++)
+				{
+					bool flag = true;
+					if (pattern.Length == words[i].Length)
+					{
+						for (int j = 0; j < pattern.Length; j++)
+						{
+							if (pattern[j] != '-' && !words[i].Contains(pattern[j]))
+							{
+								flag = false;
+							}
+						}
+						if (flag)
+							Console.WriteLine(words[i]);
+					}
+				}
+			}
+
+
+
+
+		}
+	}
+}
